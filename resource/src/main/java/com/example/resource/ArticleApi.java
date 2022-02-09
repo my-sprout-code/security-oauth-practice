42e923bb183230ccfbd35e6ec78f34ae88808312
Kim Woojae <kimwoojae@Kimui-MacBookPro.local>
2022-02-09 15:21:21 +0900

    init

---
+++ resource/src/main/java/com/example/resource/ArticleApi.java
@@ -0,0 +1,13 @@
+package com.example.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArticleApi {
+
+    @GetMapping("/articles")
+    public String[] getArticles() {
+        return new String[] { "Article 1", "Article 2", "Article 3" };
+    }
+}
